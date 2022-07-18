d7318cce3f2c3de7ab315fb4b1242695aa6e493e
Nicola Milne <nicolalouisemilne@gmail.com>
2022-07-18 10:09:01 -0600

    commit tree b6e143b48932c71289ed5a71dd1990b72f591cd9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name:"Charlie", last_name:"Felsey", grade:5)
